89a0fc03c46b9310141870ff346c92f9a15d51cd
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2022-09-21 21:29:27 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import sklearn
+
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import optimizers
+
+from keras import backend as K
+
+model = keras.models.Sequential()
+
+print("we did it") 
