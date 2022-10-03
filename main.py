79564e59346a5b2e0004d8bc78741db04460f02f
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2022-10-03 13:18:05 -0700

    updated main.py
    very excited to make a breakthrough in the model department

--- main.py
+++ main.py
@@ -23,10 +23,16 @@ image = np.zeros((height,width,depth))
 normalized_image = np.divide(image, 255)
 flattened_image = np.reshape(normalized_image, [-1,np.product(normalized_image.shape)])
 
-#Model Section
-#model = keras.models.Sequential()
-#model.add()
-
 print(image.shape)
 print(normalized_image.shape)
 print(flattened_image.shape)
+
+def make_model(image_resolution, informational_neurons, outputs)
+    image_data = keras.Input(shape=(image_resolution), name="image_data")
+    information = keras.Input(shape=(informational_neurons), name="information")
+
+    #input image_data into cnn
+
+    #input information and flattened image_data layer into dense layer
+
+    #get output
