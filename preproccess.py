80c68facaa96e4c58f4d7b961fb6f5a0bbfbcc08
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2022-10-03 10:43:24 -0700

    updated preprocess.py
    used np.zeros  instead of np.array, also got rid of unnecessary  imports

--- preproccess.py
+++ preproccess.py
@@ -2,12 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 
-image = np.array()
+image = np.zeros((320, 240, 3))
 normalized_image = np.divide(image, 255)
 flattened_image = np.reshape(normalized_image, [-1,np.product(normalized_image.shape)])
 
 print(image.shape)
 print(normalized_image.shape)
 print(flattened_image.shape)
-
 
