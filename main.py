d641ebe96f033bb148f5ed80951500f67a759a0f
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2022-10-03 10:55:13 -0700

    updated main.py

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ from keras import optimizers
 
 from keras import backend as K
 
+#Image properties
+height, width, depth = 320, 240, 3
 
-height, width = 1, 1
-inputShape = (height, width)
-model = keras.models.Sequential()
+#Template for image
+image = np.zeros((height,width,depth))
+normalized_image = np.divide(image, 255)
+flattened_image = np.reshape(normalized_image, [-1,np.product(normalized_image.shape)])
 
-model.add()
+#Model Section
+#model = keras.models.Sequential()
+#model.add()
+
+print(image.shape)
+print(normalized_image.shape)
+print(flattened_image.shape)
