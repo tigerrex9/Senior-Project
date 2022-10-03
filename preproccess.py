ccff638bd2ec87ec9a33e78372c65cc396121591
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2022-10-03 10:33:43 -0700

    Add files via upload

---
+++ preproccess.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+import numpy as np
+
+image = np.array()
+normalized_image = np.divide(image, 255)
+flattened_image = np.reshape(normalized_image, [-1,np.product(normalized_image.shape)])
+
+print(image.shape)
+print(normalized_image.shape)
+print(flattened_image.shape)
+
+
